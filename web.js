7640f9ac6c284e378963e4913e197b6714e6af3a
Alex Lucas <arglucas@hotmail.com>
2013-07-18 14:28:33 +0000

    Changed tempate to include TC content.

--- web.js
+++ web.js
@@ -9,6 +9,8 @@ app.get('/', function(request, response) {
   response.send(html);
 });
 
+app.use('/img', express.static(__dirname + '/img'));
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
